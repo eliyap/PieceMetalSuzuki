5047e6eb06a5a0d40b45c50ac11038b6fa5c5e0d
eliyap <7846005+eliyap@users.noreply.github.com>
2023-02-09 08:20:22 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PieceMetalSuzuki
+
+A description of this package.
---
+++ Sources/PieceMetalSuzuki/PieceMetalSuzuki.swift
@@ -0,0 +1,6 @@
+public struct PieceMetalSuzuki {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
