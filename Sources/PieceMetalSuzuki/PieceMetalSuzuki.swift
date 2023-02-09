e7d328ec8028c75074b99ce652e0580ed819c7e7
eliyap <7846005+eliyap@users.noreply.github.com>
2023-02-09 08:56:09 -0800

    load image into buffer

--- Sources/PieceMetalSuzuki/PieceMetalSuzuki.swift
+++ Sources/PieceMetalSuzuki/PieceMetalSuzuki.swift
@@ -1,5 +1,6 @@
 import Foundation
 import CoreImage
+import CoreVideo
 
 public struct PieceMetalSuzuki {
     public private(set) var text = "Hello, World!"
@@ -12,6 +13,46 @@ public struct PieceMetalSuzuki {
             assert(false, "Couldn't load image.")
             return
         }
+        
+        /// Make a pixel buffer.
+        let width = Int(ciImage.extent.width)
+        let height = Int(ciImage.extent.height)
+        let format = kCVPixelFormatType_32BGRA
+        let options: NSDictionary = [
+            kCVPixelBufferCGImageCompatibilityKey: true,
+        ]
+        var pxbuffer: CVPixelBuffer!
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, width, height, format, options, &pxbuffer)
+        guard status == kCVReturnSuccess else {
+            assert(false, "Failed to create pixel buffer.")
+            return
+        }
+
+        /// Copy image to pixel buffer.
+        let context = CIContext()
+        context.render(ciImage, to: pxbuffer)
+
+        /// Read values from pixel buffer.
+        CVPixelBufferLockBaseAddress(pxbuffer, [])
+        defer {
+            CVPixelBufferUnlockBaseAddress(pxbuffer, [])
+        }
+        guard let baseAddress = CVPixelBufferGetBaseAddress(pxbuffer) else {
+            assert(false, "Failed to get base address.")
+            return
+        }
+
+        let (row, col) = (75, 575)
+        let offset = (row * width + col) * 4
+
+
+        let b = baseAddress.load(fromByteOffset: offset + 0, as: UInt8.self)
+        let g = baseAddress.load(fromByteOffset: offset + 1, as: UInt8.self)
+        let r = baseAddress.load(fromByteOffset: offset + 2, as: UInt8.self)
+        let a = baseAddress.load(fromByteOffset: offset + 3, as: UInt8.self)
+        print("b: \(b), g: \(g), r: \(r), a: \(a)")
+
+        
         print("so far so good")
     }
 }
