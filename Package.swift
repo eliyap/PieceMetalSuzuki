83ea820b24e98fd565dfd94b8488bc9f55f1ff82
eliyap <7846005+eliyap@users.noreply.github.com>
2023-02-09 08:31:10 -0800

    load bundled image

--- Package.swift
+++ Package.swift
@@ -20,7 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PieceMetalSuzuki",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .copy("input.png"),
+            ]),
         .testTarget(
             name: "PieceMetalSuzukiTests",
             dependencies: ["PieceMetalSuzuki"]),
--- Sources/PieceMetalSuzuki/PieceMetalSuzuki.swift
+++ Sources/PieceMetalSuzuki/PieceMetalSuzuki.swift
@@ -1,6 +1,17 @@
+import Foundation
+import CoreImage
+
 public struct PieceMetalSuzuki {
     public private(set) var text = "Hello, World!"
 
     public init() {
+        guard
+            let imageUrl = Bundle.module.url(forResource: "input", withExtension: ".png"),
+            let ciImage = CIImage(contentsOf: imageUrl)
+        else {
+            assert(false, "Couldn't load image.")
+            return
+        }
+        print("so far so good")
     }
 }
