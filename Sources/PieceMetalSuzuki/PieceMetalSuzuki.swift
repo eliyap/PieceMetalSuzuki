cfb27e47985f1147e6a604b2c265dcae2224cb21
eliyap <7846005+eliyap@users.noreply.github.com>
2023-02-09 16:39:14 -0800

    add doc string

--- Sources/PieceMetalSuzuki/PieceMetalSuzuki.swift
+++ Sources/PieceMetalSuzuki/PieceMetalSuzuki.swift
@@ -159,3 +159,24 @@ func makeTextureFromCVPixelBuffer(
     
     return texture
 }
+
+extension MTLComputePipelineState {
+    func threadgroupParameters(texture: MTLTexture) -> (threadgroupsPerGrid: MTLSize, threadsPerThreadgroup: MTLSize) {
+        let threadHeight = maxTotalThreadsPerThreadgroup / threadExecutionWidth
+        return (
+            /// Subdivide grid as far as possible.
+            MTLSizeMake(threadExecutionWidth, threadHeight, 1),
+            /// Via https://developer.apple.com/documentation/metal/mtlcomputecommandencoder/1443138-dispatchthreadgroups
+            /// Also seen in https://developer.apple.com/documentation/avfoundation/additional_data_capture/avcamfilter_applying_filters_to_a_capture_stream
+            ///
+            /// The weird math is a form of rounding up using integer division.
+            /// If thread `width` or `height` evenly divides the texture `width` or `height`, that factor is returned.
+            /// Otherwise, the value is increased enough to push division to the next number, effectively rounding up.
+            MTLSizeMake(
+                (texture.width  + threadExecutionWidth - 1) / threadExecutionWidth,
+                (texture.height + threadHeight         - 1) / threadHeight,
+                1
+            )
+        )
+    }
+}
