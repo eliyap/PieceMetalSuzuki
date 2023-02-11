c457d85a43effc91e59d0e32aebaf57f51f73fb2
eliyap <7846005+eliyap@users.noreply.github.com>
2023-02-11 10:16:34 -0800

    add failing test

--- Tests/PieceMetalSuzukiTests/PieceMetalSuzukiTests.swift
+++ Tests/PieceMetalSuzukiTests/PieceMetalSuzukiTests.swift
@@ -2,10 +2,25 @@ import XCTest
 @testable import PieceMetalSuzuki
 
 final class PieceMetalSuzukiTests: XCTestCase {
+    
+    func url(_ name: String) -> URL {
+        Bundle.module.url(forResource: name, withExtension: ".png", subdirectory: "Images")!
+    }
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        PieceMetalSuzuki(imageUrl: Bundle.module.url(forResource: "input", withExtension: ".png", subdirectory: "Images")!)
+        PieceMetalSuzuki(imageUrl: url("waffle"))
+//        PieceMetalSuzuki(imageUrl: url("input"))
+        
+    }
+    
+    func testWaffle() throws {
+        _ = PieceMetalSuzuki(imageUrl: url("waffle"))
+    }
+    
+    func testWhite() throws {
+        _ = PieceMetalSuzuki(imageUrl: url("white"))
     }
 }
