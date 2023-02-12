bf3415cbb2e1619ee7d0b9b74589e423417893b7
eliyap <7846005+eliyap@users.noreply.github.com>
2023-02-12 07:58:41 -0800

    split file

--- Sources/PieceMetalSuzuki/Region.swift
+++ Sources/PieceMetalSuzuki/Region.swift
@@ -286,43 +286,6 @@ struct Grid {
         // return regions[0][0]
     }
     
-    func blit(
-        device: MTLDevice, commandQueue: MTLCommandQueue,
-        blitRunIndices: [Int], srcRuns: UnsafeMutablePointer<Run>,
-        srcPts: Buffer<PixelPoint>, dstPts: Buffer<PixelPoint>,
-        cpu: Bool = true
-    ) -> Bool {
-        if cpu {
-            cpuBlit(runIndices: blitRunIndices, srcPts: srcPts.array, srcRuns: srcRuns, dstPts: dstPts.array)
-            return true
-        } else {
-            guard let cmdBuffer = commandQueue.makeCommandBuffer() else {
-                assert(false, "Failed to create command buffer.")
-                return false
-            }
-            for request in blitRunIndices {
-                guard let cmdEncoder = cmdBuffer.makeBlitCommandEncoder() else {
-                    assert(false, "Failed to create command encoder.")
-                    return false
-                }
-                let run = srcRuns[request]
-                cmdEncoder.copy(
-                    from: srcPts.mtlBuffer, sourceOffset: MemoryLayout<PixelPoint>.stride * Int(run.oldTail),
-                    to: dstPts.mtlBuffer, destinationOffset: MemoryLayout<PixelPoint>.stride * Int(run.newTail),
-                    size: MemoryLayout<PixelPoint>.stride * Int(run.oldHead - run.oldTail)
-                )
-                cmdEncoder.endEncoding()
-            }
-            cmdBuffer.commit()
-            
-            Profiler.time(.blitWait) {
-                cmdBuffer.waitUntilCompleted()
-            }
-            
-            return true
-        }
-    }
-    
     #if SHOW_GRID_WORK
     func dump(region: Region, points: UnsafeMutablePointer<PixelPoint>, runs: UnsafeMutablePointer<Run>) {
         let baseOffset = baseOffset(grid: self, region: region)
@@ -584,23 +547,3 @@ func baseOffset(grid: Grid, region: Region) -> UInt32 {
     baseOffset(imageSize: grid.imageSize, gridSize: grid.gridSize, regionSize: region.size, gridPos: region.gridPos)
 }
 
-#warning("TEMP: CPU BLIT")
-/// For each source run, copy its points to the destination.
-func cpuBlit(
-    runIndices: [Int],
-    srcPts: UnsafeMutablePointer<PixelPoint>, srcRuns: UnsafeMutablePointer<Run>,
-    dstPts: UnsafeMutablePointer<PixelPoint>
-) -> Void {
-    DispatchQueue.concurrentPerform(iterations: runIndices.count) { runIdxIdx in
-        let runIdx = runIndices[runIdxIdx]
-        #if SHOW_GRID_WORK
-        debugPrint("[BLIT] \(run)")
-        #endif
-        let run = srcRuns[runIdx]
-        memmove(
-            dstPts.advanced(by: Int(run.newTail)),
-            srcPts.advanced(by: Int(run.oldTail)),
-            MemoryLayout<PixelPoint>.stride * Int(run.oldHead - run.oldTail)
-        )
-    }
-}
