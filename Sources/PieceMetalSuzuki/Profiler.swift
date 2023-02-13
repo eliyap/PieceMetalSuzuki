212d6e1f4950a062da373ee1fcc024a91113e65d
eliyap <7846005+eliyap@users.noreply.github.com>
2023-02-13 12:20:53 -0800

    split file

---
+++ Sources/PieceMetalSuzuki/Profiler.swift
@@ -0,0 +1,100 @@
+//
+//  Profiler.swift
+//  
+//
+//  Created by Secret Asian Man Dev on 13/2/23.
+//
+
+import Foundation
+
+extension Dictionary where Key: Comparable {
+    func sortedByKey() -> [(Key, Value)] {
+        self.sorted { (arg1, arg2) in
+            let (k2, _) = arg2
+            let (k1, _) = arg1
+            return k1 < k2
+        }
+    }
+}
+
+public actor Profiler {
+    enum CodeRegion: CaseIterable {
+        case blit, trailingCopy, binarize, startChains, overall, makeTexture, initRegions, bufferInit, blitWait, runIndices
+        case combineAll, combine, combineFindPartner, combineJoin
+    }
+
+    private var timing: [CodeRegion: (Int, TimeInterval)] = {
+        var dict = [CodeRegion: (Int, TimeInterval)]()
+        for region in CodeRegion.allCases {
+            dict[region] = (0, 0)
+        }
+        return dict
+    }()
+    
+    private var iterationTiming: [Int: (Int, TimeInterval)] = {
+        var dict = [Int: (Int, TimeInterval)]()
+        return dict
+    }()
+
+    private init() { }
+    public static let shared = Profiler()
+
+    private func add(_ duration: TimeInterval, to region: CodeRegion) -> Void {
+        let (count, total) = timing[region]!
+        timing[region] = (count + 1, total + duration)
+    }
+    static func add(_ duration: TimeInterval, to region: CodeRegion) {
+        Task(priority: .high) {
+            await Profiler.shared.add(duration, to: region)
+        }
+    }
+    
+    private func add(_ duration: TimeInterval, iteration: Int) -> Void {
+        let (count, total) = iterationTiming[iteration] ?? (0, 0)
+        iterationTiming[iteration] = (count + 1, total + duration)
+    }
+    static func add(_ duration: TimeInterval, iteration: Int) -> Void {
+        Task(priority: .high) {
+            await Profiler.shared.add(duration, iteration: iteration)
+        }
+    }
+
+    static func time(_ region: CodeRegion, _ block: () -> Void) {
+        let start = CFAbsoluteTimeGetCurrent()
+        block()
+        let end = CFAbsoluteTimeGetCurrent()
+        Profiler.add(end - start, to: region)
+    }
+    
+    static func time(_ iteration: Int, _ block: () -> Void) {
+        let start = CFAbsoluteTimeGetCurrent()
+        block()
+        let end = CFAbsoluteTimeGetCurrent()
+        Task(priority: .high) {
+            await Profiler.shared.add(end - start, iteration: iteration)
+        }
+    }
+    
+    static func time<Result>(_ region: CodeRegion, _ block: () -> Result) -> Result {
+        let start = CFAbsoluteTimeGetCurrent()
+        let result = block()
+        let end = CFAbsoluteTimeGetCurrent()
+        Profiler.add(end - start, to: region)
+        return result
+    }
+
+    static func report() async {
+        let dict = await Profiler.shared.timing
+        for (region, results) in dict where [.overall, .combineAll, .combine].contains(region) {
+//            for (region, results) in dict where results.0 > 0 {
+            let (count, time) = results
+            print("\(region): \(time)s, \(count) (avg \(time / Double(count))s)")
+        }
+        
+        let timingDict = await Profiler.shared.iterationTiming
+        for (iteration, results) in timingDict.sortedByKey() {
+            let (count, time) = results
+            print("\(iteration): \(time)s, \(count) (avg \(time / Double(count))s)")
+        }
+    }
+}
