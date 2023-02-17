002ae42b699e53d9062b883ea5bdb2211e61f3fa
eliyap <7846005+eliyap@users.noreply.github.com>
2023-02-16 16:40:24 -0800

    clone function

--- Sources/PieceMetalSuzuki/LookupTables/MTLMatchPattern.swift
+++ Sources/PieceMetalSuzuki/LookupTables/MTLMatchPattern.swift
@@ -7,6 +7,43 @@
 
 import Metal
 
+func matchPatterns(
+    device: MTLDevice,
+    commandQueue: MTLCommandQueue,
+    texture: MTLTexture,
+    runBuffer: Buffer<Run>,
+    pointBuffer: Buffer<PixelPoint>
+) -> Bool {
+    guard
+        let kernelFunction = loadMatchPatternFunction(device: device),
+        let pipelineState = try? device.makeComputePipelineState(function: kernelFunction),
+        let cmdBuffer = commandQueue.makeCommandBuffer(),
+        let cmdEncoder = cmdBuffer.makeComputeCommandEncoder()
+    else {
+        assert(false, "Failed to setup pipeline.")
+        return false
+    }
+    
+    cmdEncoder.label = "Custom Kernel Encoder"
+    cmdEncoder.setComputePipelineState(pipelineState)
+    cmdEncoder.setTexture(texture, index: 0)
+
+    cmdEncoder.setBuffer(pointBuffer.mtlBuffer, offset: 0, index: 0)
+    cmdEncoder.setBuffer(runBuffer.mtlBuffer, offset: 0, index: 1)
+    cmdEncoder.setBuffer(StartRun.lookupTable.mtlBuffer, offset: 0, index: 2)
+    cmdEncoder.setBuffer(StartRun.lookupTableIndices.mtlBuffer, offset: 0, index: 3)
+    cmdEncoder.setBuffer(StartPoint.lookupTable.mtlBuffer, offset: 0, index: 4)
+    cmdEncoder.setBuffer(StartPoint.lookupTableIndices.mtlBuffer, offset: 0, index: 5)
+    
+    let (tPerTG, tgPerGrid) = pipelineState.threadgroupParameters(texture: texture)
+    cmdEncoder.dispatchThreadgroups(tgPerGrid, threadsPerThreadgroup: tPerTG)
+    cmdEncoder.endEncoding()
+    cmdBuffer.commit()
+    cmdBuffer.waitUntilCompleted()
+    
+    return true
+}
+
 func loadMatchPatternFunction(device: MTLDevice) -> MTLFunction? {
     do {
         guard let libUrl = Bundle.module.url(forResource: "MatchPattern", withExtension: "metal", subdirectory: "Metal") else {
