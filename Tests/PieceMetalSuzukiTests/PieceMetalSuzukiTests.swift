bc836ebfab8a6d0679d898cbcb59e2094b5fec43
eliyap <7846005+eliyap@users.noreply.github.com>
2023-02-20 15:42:21 -0800

    devise a marker script

--- Tests/PieceMetalSuzukiTests/PieceMetalSuzukiTests.swift
+++ Tests/PieceMetalSuzukiTests/PieceMetalSuzukiTests.swift
@@ -118,7 +118,36 @@ final class PieceMetalSuzukiTests: XCTestCase {
     func testRDP() throws {
         let patternSize = PatternSize.w2h2
         loadLookupTables(patternSize)
-        _ = PieceMetalSuzuki(imageUrl: url("qr"), patternSize: patternSize) { device, queue, texture, pointsFilled, runsFilled, pointsUnfilled, runsUnfilled in
+        
+        let imageUrl = url("qrTilt")
+        
+        let ciImage = CIImage(contentsOf: imageUrl)!
+        
+        /// Make a pixel buffer.
+        var width = Int(ciImage.extent.width)
+        let height = Int(ciImage.extent.height)
+        let format = kCVPixelFormatType_32BGRA
+        let options: NSDictionary = [
+            kCVPixelBufferCGImageCompatibilityKey: true,
+            kCVPixelBufferMetalCompatibilityKey: true,
+        ]
+        var buffer: CVPixelBuffer!
+        CVPixelBufferCreate(kCFAllocatorDefault, width, height, format, options, &buffer)
+
+        /// Copy image to pixel buffer.
+        let context = CIContext()
+        context.render(ciImage, to: buffer)
+        
+        var (xLeft, xRight, yTop, yBottom) = (0, 0, 0, 0)
+        CVPixelBufferGetExtendedPixels(buffer, &xLeft, &xRight, &yTop, &yBottom)
+        debugPrint("xLeft: \(xLeft), xRight: \(xRight), yTop: \(yTop), yBottom: \(yBottom)")
+
+        width += 14
+
+        CVPixelBufferLockBaseAddress(buffer, CVPixelBufferLockFlags(rawValue: 0))
+        
+        
+        _ = PieceMetalSuzuki(imageUrl: imageUrl, patternSize: patternSize) { device, queue, texture, pointsFilled, runsFilled, pointsUnfilled, runsUnfilled in
             let runIndices = applyMetalSuzuki_LUT(device: device, commandQueue: queue, texture: texture, pointsFilled: pointsFilled, runsFilled: runsFilled, pointsUnfilled: pointsUnfilled, runsUnfilled: runsUnfilled, patternSize: patternSize)!
             for runIdx in runIndices {
                 let run = runsFilled.array[runIdx]
@@ -127,11 +156,29 @@ final class PieceMetalSuzukiTests: XCTestCase {
                     return DoublePoint(pixelPt)
                 }
                 
-                if approximate(polyline: points) {
-                    print("Run \(runIdx) has \(points.count) points")
+                guard runIdx == 121 else { continue }
+                guard checkQuadrangle(polyline: points) else{
+                    continue
                 }
+
+                print("Run \(runIdx) has \(points.count) points")
                 
+                let addr = CVPixelBufferGetBaseAddress(buffer)!
+                    .assumingMemoryBound(to: UInt8.self)
+                (run.oldTail..<run.oldHead).forEach { ptIdx in
+                    let pixelPt = pointsFilled.array[Int(ptIdx)]
+                    // Mark pixel.
+                    let offset = (Int(pixelPt.y) * width + Int(pixelPt.x)) * 4
+                    let pixel = addr.advanced(by: offset)
+                    pixel[0] = 0
+                    pixel[1] = 0
+                    pixel[2] = 255
+                    pixel[3] = 255
+                }
             }
         }
+        
+        CVPixelBufferUnlockBaseAddress(buffer, CVPixelBufferLockFlags(rawValue: 0))
+        saveBufferToPng(buffer: buffer, format: .RGBA8)
     }
 }
