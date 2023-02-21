b1a5f8473d583400838c2538b420c5379830a4e0
eliyap <7846005+eliyap@users.noreply.github.com>
2023-02-21 10:40:31 -0800

    split file

--- Sources/PieceMetalSuzuki/RDP.swift
+++ Sources/PieceMetalSuzuki/RDP.swift
@@ -21,45 +21,6 @@ public struct RDPParameters {
     )
 }
 
-public struct DoublePoint: Equatable {
-    let x: Double
-    let y: Double
-    public init(_ pt: PixelPoint) {
-        self.x = Double(pt.x)
-        self.y = Double(pt.y)
-    }
-    public init(x: Double, y: Double) {
-        self.x = x
-        self.y = y
-    }
-    
-    /// Source: https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
-    /// Signed distance above or below a line.
-    /// Line defined by p0, p1.
-    internal func displacement(p0: DoublePoint, p1: DoublePoint) -> Double {
-        assert(p0 != p1)
-        let a = (p1.x - p0.x) * (p0.y - self.y)
-        let b = (p1.y - p0.y) * (p0.x - self.x)
-        let dx2 = (p1.x - p0.x) * (p1.x - p0.x)
-        let dy2 = (p1.y - p0.y) * (p1.y - p0.y)
-        return (a - b) / sqrt(dx2 + dy2)
-    }
-
-    /// Absolute distance from a line.
-    /// Line defined by p0, p1.
-    internal func distance(p0: DoublePoint, p1: DoublePoint) -> Double {
-        return abs(displacement(p0: p0, p1: p1))
-    }
-
-    internal func distance(to other: DoublePoint) -> Double {
-        let dx = self.x - other.x
-        let dy = self.y - other.y
-        return sqrt((dx * dx) + (dy * dy))
-    }
-}
-
-
-
 public func approximate(polyline: [DoublePoint], parameters: RDPParameters = .starter) -> Bool {
     guard polyline.count > parameters.minPoints else {
         return false
