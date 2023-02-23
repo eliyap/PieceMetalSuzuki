53a7c95edb91720692cef10e651a182c39fe22ef
eliyap <7846005+eliyap@users.noreply.github.com>
2023-02-22 16:53:29 -0800

    declare struct

---
+++ Sources/PieceMetalSuzuki/DecodeMarker/Quadrilateral.swift
@@ -0,0 +1,15 @@
+//
+//  Quadrilateral.swift
+//  Aruco
+//
+//  Created by Secret Asian Man Dev on 22/2/23.
+//
+
+import Foundation
+
+struct Quadrilateral { 
+    public let corner1: DoublePoint
+    public let corner2: DoublePoint
+    public let corner3: DoublePoint
+    public let corner4: DoublePoint
+}
