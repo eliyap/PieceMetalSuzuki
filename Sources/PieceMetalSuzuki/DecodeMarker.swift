65741404d52fe2f03f1ae2779cc08cece27286a9
eliyap <7846005+eliyap@users.noreply.github.com>
2023-02-23 10:15:30 -0800

    strip debugging code

--- Sources/PieceMetalSuzuki/DecodeMarker.swift
+++ Sources/PieceMetalSuzuki/DecodeMarker.swift
@@ -18,7 +18,6 @@ internal func decodeMarkers(
     CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
     let addr = CVPixelBufferGetBaseAddress(pixelBuffer)!
         .assumingMemoryBound(to: UInt8.self)
-    let bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer)
     
     for runIdx in runIndices {
         let run = runBuffer.array[runIdx]
@@ -30,27 +29,6 @@ internal func decodeMarkers(
         guard let quad = checkQuadrilateral(polyline: points) else {
             continue
         }
-        
-        (run.oldTail..<run.oldHead).forEach { ptIdx in
-            let pixelPt = pointBuffer.array[Int(ptIdx)]
-            // Mark pixel.
-            let offset = (Int(pixelPt.y) * bytesPerRow) + (Int(pixelPt.x) * 4)
-            let pixel = addr.advanced(by: offset)
-            pixel[0] = 0
-            pixel[1] = 0
-            pixel[2] = 255
-            pixel[3] = 255
-        }
-        
-        guard let m = matrixFor(quadrilateral: quad) else {
-            debugPrint("Singular matrix")
-            continue
-        }
-        
-        print("c1", quad.corner1.transformed(by: m))
-        print("c2", quad.corner2.transformed(by: m))
-        print("c3", quad.corner3.transformed(by: m))
-        print("c4", quad.corner4.transformed(by: m))
         
         let samples = sampleSkewedGrid(
             pixelBuffer: pixelBuffer,
@@ -64,5 +42,4 @@ internal func decodeMarkers(
         }
     }
     CVPixelBufferUnlockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
-    saveBufferToPng(buffer: pixelBuffer, format: .RGBA8)
 }
