b2ae682ef928cc04dd99e256cf9b42c218620d73
eliyap <7846005+eliyap@users.noreply.github.com>
2023-03-02 08:22:44 -0800

    add Swift Protobuf dependency

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.21.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,6 +28,7 @@ let package = Package(
             name: "PieceMetalSuzuki",
             dependencies: [
                 .product(name: "Collections", package: "swift-collections"),
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
             ],
             resources: [
                 .copy("Images/"),
