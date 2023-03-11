e47ad650a56fe9f8890583748cbccaa06ebb74ef
eliyap <7846005+eliyap@users.noreply.github.com>
2023-03-11 11:10:24 -0800

    add doc comments

--- Sources/PieceMetalSuzuki/LookupTables/PatternSize.swift
+++ Sources/PieceMetalSuzuki/LookupTables/PatternSize.swift
@@ -11,7 +11,7 @@ import Foundation
  A "pattern" is an arrangement of initial on / off pixels.
  It consists of
  - a core: the pixels we're interested in, which the `Region` covers
- - a perimeter: the immediate surroundings, which inform how this `Region` will combine with adjoining ones.
+ - a perimeter: the immediate surroundings, which inform how this `Region` will combine with adjacent `Region`s
  
  For example, here's a simple 3x3 pattern, with a 1x1 core:
  ```
@@ -21,6 +21,8 @@ import Foundation
  ```
  This pattern produces 4 distinct triads; the most any 3x3 pattern can have.
  Hence, the 3x3 lookup table has 4 columns.
+ For more information on triads, see paper:
+ https://link.springer.com/article/10.1007/s11227-021-04260-y
  
  However,  consider the following pattern with a 2x1 core:
  ```
@@ -55,18 +57,37 @@ extension PatternSize {
     public static let w1h1 = PatternSize(
         coreSize: PixelSize(width: 1, height: 1),
         tableWidth: 4,
+        /// Densest pattern, 4 triads / 1 pixel, `X` shape.
+        /// ```
+        /// 101 \ /
+        /// 010  X
+        /// 101 / \
+        /// ```
         pointsPerPixel: 4
     )
     
     public static let w2h1 = PatternSize(
         coreSize: PixelSize(width: 2, height: 1),
         tableWidth: 6,
+        /// Densest pattern, 6 triads / 2 pixels, `>-<` shape.
+        /// ```
+        /// 1001 \  /
+        /// 0110  ><
+        /// 1001 /  \
+        /// ```
         pointsPerPixel: 3
     )
     
     public static let w2h2 = PatternSize(
         coreSize: PixelSize(width: 2, height: 2),
         tableWidth: 8,
+        /// Densest patterns, 8 triads / 4 pixels, `XX` shape, or a big `X`.
+        /// ```
+        /// 1010 \ ^     1001 \  /
+        /// 0101  X >    0110  ++
+        /// 1010 < X     0110  ++
+        /// 0101  v \    1001 /  \
+        /// ```
         pointsPerPixel: 2
     )
 }
