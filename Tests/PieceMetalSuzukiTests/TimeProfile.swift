ebb7193c78522240eb027a975c2ba26255c6ab07
eliyap <7846005+eliyap@users.noreply.github.com>
2023-03-15 09:33:33 -0700

    increase count

--- Tests/PieceMetalSuzukiTests/TimeProfile.swift
+++ Tests/PieceMetalSuzukiTests/TimeProfile.swift
@@ -21,7 +21,7 @@ final class SuzukiTimeProfile: XCTestCase {
         assert(loadLookupTablesData(patternSize))
         
         let options = SuzukiTimeProfile.defaultMeasureOptions
-        options.iterationCount = 10
+        options.iterationCount = 100
         measure(options: options) {
             _ = PieceMetalSuzuki(imageUrl: url("bigDots"), patternSize: patternSize, format: kCVPixelFormatType_32BGRA) { device, queue, texture, pixelBuffer in
                 _ = applyMetalSuzuki_LUT(device: device, commandQueue: queue, texture: texture, patternSize: patternSize)
