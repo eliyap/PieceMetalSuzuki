28cdb6f7bb23967d1ae3d81f403a1f01ed40910b
eliyap <7846005+eliyap@users.noreply.github.com>
2023-03-15 07:26:02 -0700

    test multiple sizes

--- Tests/PieceMetalSuzukiTests/SmallPattern.swift
+++ Tests/PieceMetalSuzukiTests/SmallPattern.swift
@@ -3,7 +3,11 @@ import XCTest
 
 final class SmallPatternTests: XCTestCase {
     
-    let patternSize = PatternSize.w2h2
+    let patternSizes = [
+        PatternSize.w2h1,
+        PatternSize.w2h2,
+        PatternSize.w4h2,
+    ]
     
     func url(_ name: String) -> URL {
         Bundle.module.url(forResource: name, withExtension: ".png", subdirectory: "Images")!
@@ -42,44 +46,58 @@ final class SmallPatternTests: XCTestCase {
     }
     
     func testWaffle() throws {
-        try checkPatternCountNoLUT(name: "waffle", expectedCount: 5)
-        try checkPatternCountProtoBuf(name: "waffle", expectedCount: 5, patternSize: patternSize)
-        try checkPatternCountData(name: "waffle", expectedCount: 5, patternSize: patternSize)
+        for patternSize in patternSizes {
+            try checkPatternCountNoLUT(name: "waffle", expectedCount: 5)
+            try checkPatternCountProtoBuf(name: "waffle", expectedCount: 5, patternSize: patternSize)
+            try checkPatternCountData(name: "waffle", expectedCount: 5, patternSize: patternSize)
+        }
     }
     
     func testWhite() throws {
-        try checkPatternCountNoLUT(name: "white", expectedCount: 1)
-        try checkPatternCountProtoBuf(name: "white", expectedCount: 1, patternSize: patternSize)
-        try checkPatternCountData(name: "white", expectedCount: 1, patternSize: patternSize)
+        for patternSize in patternSizes {
+            try checkPatternCountNoLUT(name: "white", expectedCount: 1)
+            try checkPatternCountProtoBuf(name: "white", expectedCount: 1, patternSize: patternSize)
+            try checkPatternCountData(name: "white", expectedCount: 1, patternSize: patternSize)
+        }
     }
     
     func testDots() throws {
-        try checkPatternCountNoLUT(name: "dots", expectedCount: 0)
-        try checkPatternCountProtoBuf(name: "dots", expectedCount: 0, patternSize: patternSize)
-        try checkPatternCountData(name: "dots", expectedCount: 0, patternSize: patternSize)
+        for patternSize in patternSizes {
+            try checkPatternCountNoLUT(name: "dots", expectedCount: 0)
+            try checkPatternCountProtoBuf(name: "dots", expectedCount: 0, patternSize: patternSize)
+            try checkPatternCountData(name: "dots", expectedCount: 0, patternSize: patternSize)
+        }
     }
     
     func testDiamonds() throws {
-        try checkPatternCountNoLUT(name: "diamonds", expectedCount: 5)
-        try checkPatternCountProtoBuf(name: "diamonds", expectedCount: 5, patternSize: patternSize)
-        try checkPatternCountData(name: "diamonds", expectedCount: 5, patternSize: patternSize)
+        for patternSize in patternSizes {
+            try checkPatternCountNoLUT(name: "diamonds", expectedCount: 5)
+            try checkPatternCountProtoBuf(name: "diamonds", expectedCount: 5, patternSize: patternSize)
+            try checkPatternCountData(name: "diamonds", expectedCount: 5, patternSize: patternSize)
+        }
     }
     
     func testSquare() throws {
-        try checkPatternCountNoLUT(name: "square", expectedCount: 2)
-        try checkPatternCountProtoBuf(name: "square", expectedCount: 2, patternSize: patternSize)
-        try checkPatternCountData(name: "square", expectedCount: 2, patternSize: patternSize)
+        for patternSize in patternSizes {
+            try checkPatternCountNoLUT(name: "square", expectedCount: 2)
+            try checkPatternCountProtoBuf(name: "square", expectedCount: 2, patternSize: patternSize)
+            try checkPatternCountData(name: "square", expectedCount: 2, patternSize: patternSize)
+        }
     }
     
     func testDonut() throws {
-        try checkPatternCountNoLUT(name: "donut", expectedCount: 4)
-        try checkPatternCountProtoBuf(name: "donut", expectedCount: 4, patternSize: patternSize)
-        try checkPatternCountData(name: "donut", expectedCount: 4, patternSize: patternSize)
+        for patternSize in patternSizes {
+            try checkPatternCountNoLUT(name: "donut", expectedCount: 4)
+            try checkPatternCountProtoBuf(name: "donut", expectedCount: 4, patternSize: patternSize)
+            try checkPatternCountData(name: "donut", expectedCount: 4, patternSize: patternSize)
+        }
     }
     
     func testBigDots() throws {
-        try checkPatternCountNoLUT(name: "bigDots", expectedCount: nil)
-        try checkPatternCountProtoBuf(name: "bigDots", expectedCount: nil, patternSize: patternSize)
-        try checkPatternCountData(name: "bigDots", expectedCount: nil, patternSize: patternSize)
+        for patternSize in patternSizes {
+            try checkPatternCountNoLUT(name: "bigDots", expectedCount: nil)
+            try checkPatternCountProtoBuf(name: "bigDots", expectedCount: nil, patternSize: patternSize)
+            try checkPatternCountData(name: "bigDots", expectedCount: nil, patternSize: patternSize)
+        }
     }
 }
